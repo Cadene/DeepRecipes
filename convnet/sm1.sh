26eb7703510624c847cfdd13ed0ea72310301808
Tamazy <tamazy@laposte.net>
2015-07-20 20:33:53 +0200

    SGD

--- convnet/sm1.sh
+++ convnet/sm1.sh
@@ -1,19 +1,38 @@
 th -i main.lua \
-    -optimizer ADAGRAD \
-    -learning_rate 1e-1 \
-    -criterion NLL \
-    -plot_every 2000 \
-    -batch_size 200 \
-    -epoch 1000 \
-    -save_every 1 \
-    -load_model false \
-    -path2model ./sm1/cade.net \
-    -model_type overfeat \
+\
     -data_type Recipe101 \
     -path2dir /Users/remicadene/data/recipe_101_low/ \
+\
+    -pc_train 0.8 \
+\
+    -load_seed true \
+    -path2seed ./sm1/seed.save \
+\
+    -load_model true \
+    -path2model ./sm1/cade.net \
+\
+    -type float \
+    -threads 8 \
+    -gpuid 1 \
+    -model_type overfeat \
+    -criterion NLL \
+    -dropout 0.5 \
+\
+    -optimizer SGD \
+    -learning_rate 5e-3 \
+    -learning_rate_decay 0.033 \
+    -momentum 0.06 \
+    -weight_decay 1e-5 \
+    -batch_size 128 \
+\
+    -epoch 1000 \
+    -path2save ./sm1/ \
+    -plot_every 2000 \
+    -save_every 1 \
+\
     -run true \
     -train true \
     -test true \
-    -path2save ./sm1/ \
-    -load_seed false \
-    -path2seed ./sm1/seed.save
+    
+    
+    
