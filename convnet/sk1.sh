e4477f7283e1f1fb31df9d654d1fd62a6a6aa7a1
Tamazy <tamazy@laposte.net>
2015-07-21 23:20:01 +0200

    sync

--- convnet/sk1.sh
+++ convnet/sk1.sh
@@ -5,10 +5,10 @@ th -i main.lua \
 \
     -pc_train 0.8 \
 \
-    -load_seed true \
+    -load_seed false \
     -path2seed ./sk1/seed.save \
 \
-    -load_model true \
+    -load_model false \
     -path2model ./sk1/cade.net \
 \
     -type cuda \
--- convnet/sk5.sh
+++ convnet/sk5.sh
@@ -5,7 +5,7 @@ th -i main.lua \
 \
     -pc_train 0.8 \
 \
-    -load_seed true \
+    -load_seed false \
     -path2seed ./sk2/seed.save \
 \
     -load_model false \
@@ -19,14 +19,14 @@ th -i main.lua \
     -dropout 0.5 \
 \
     -optimizer SGD \
-    -learning_rate 5e-2 \
+    -learning_rate 5e-3 \
     -learning_rate_decay 0.033 \
     -momentum 0.06 \
     -weight_decay 1e-5 \
     -batch_size 128 \
 \
     -epoch 1000 \
-    -path2save ./sk3/ \
+    -path2save ./sk5/ \
     -plot_every 2000 \
     -save_every 1 \
 \
