9c09c87dcf8a82c59da9310dd4f46a2afe49e3fe
Tamazy <tamazy@laposte.net>
2015-07-23 23:48:10 +0200

    batchsize

--- convnet/sk6.sh
+++ convnet/sk6.sh
@@ -18,12 +18,7 @@ th -i main.lua \
     -criterion NLL \
     -dropout 0.5 \
 \
-    '#-optimizer SGD \
-    -learning_rate 5e-2 \
-    -learning_rate_decay 0.033 \
-    -momentum 0.06 \
-    -weight_decay 1e-5 \
-    -batch_size 128 \'
+    -batch_size 128 \
     -optimizer ADAGRAD \
     -learning_rate 1e-2 \
 \
