3fa4c33f9c6e7ef172e077045c37d8f27a44a17a
Tamazy <tamazy@laposte.net>
2015-07-29 16:58:59 +0200

    sync

--- convnet/sk5.sh
+++ convnet/sk5.sh
@@ -1,34 +1,35 @@
 th -i main.lua \
 \
     -data_type Recipe101 \
-    -path2dir /Users/remicadene/data/recipe_101_low/ \
+    -path2dir /home/cadene/data/recipe_101_clean/ \
 \
     -pc_train 0.8 \
 \
-    -type float \
+    -type cuda \
     -threads 8 \
-    -gpuid 1 \
+    -gpuid 5 \
     -model_type small \
     -criterion NLL \
-    -dropout 0 \
+    -dropout 0.6 \
 \
     -batch_size 128 \
     -optimizer SGD \
+    -learning_rate_decay 0 \
     -learning_rate 0 \
     -momentum 0 \
 \
     -load_model true \
-    -path2load ./sm1/ \
+    -path2load ./sk5-4/ \
     -epoch 100 \
-    -path2save ./sm1/ \
+    -path2save ./sk5-5/ \
     -plot_every 2000 \
     -plot false \
     -save_every 1 \
 \
-    -run false \
+    -run true \
     -train true \
     -test true \
-    -save false 
+    -save true 
     
     
     
