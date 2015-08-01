9be90f98e7aeda7283ec8e7eb616f050a98902f3
Tamazy <tamazy@laposte.net>
2015-08-02 00:41:11 +0200

    tiny + test sk7 debug train

--- convnet/2_model.lua
+++ convnet/2_model.lua
@@ -252,26 +252,26 @@ else
 
         model = nn.Sequential()
 
-        model:add(nn.SpatialConvolutionMM(3, 16, 3, 3, 1, 1, 1, 1))
+        model:add(nn.SpatialConvolutionMM(3, 4, 3, 3, 1, 1, 1, 1))
         model:add(nn.ReLU())
         model:add(nn.SpatialMaxPooling(2, 2, 2, 2))
 
-        model:add(nn.SpatialConvolutionMM(16, 32, 3, 3, 1, 1, 1, 1))
+        model:add(nn.SpatialConvolutionMM(4, 8, 3, 3, 1, 1, 1, 1))
         model:add(nn.ReLU())
         model:add(nn.SpatialMaxPooling(2, 2, 2, 2))
 
-        model:add(nn.SpatialConvolutionMM(32, 32, 3, 3, 1, 1, 1, 1))
+        model:add(nn.SpatialConvolutionMM(8, 8, 3, 3, 1, 1, 1, 1))
         model:add(nn.ReLU())
-        model:add(nn.SpatialMaxPooling(3, 3, 3, 3))
+        model:add(nn.SpatialMaxPooling(2, 2, 2, 2))
 
-        model:add(nn.Reshape(10368))
-        model:add(nn.Linear(10368,2048))
+        model:add(nn.Reshape(5832))
+        model:add(nn.Linear(5832,1024))
         model:add(nn.ReLU())
         if opt.dropout ~= 0 then
             model:add( nn.Dropout(opt.dropout) )
         end
 
-        model:add(nn.Linear(2048,1024))
+        model:add(nn.Linear(1024,1024))
         model:add(nn.ReLU())
         if opt.dropout ~= 0 then
             model:add( nn.Dropout(opt.dropout) )
--- convnet/sk7.sh
+++ convnet/sk7.sh
@@ -6,19 +6,20 @@ th -i main.lua \
     -pc_train 0.8 \
 \
     -type cuda \
-    -threads 1 \
+    -threads 8 \
     -gpuid 7 \
     -model_type small \
     -criterion NLL \
-    -dropout 0.6 \
+    -dropout 0 \
 \
     -batch_size 128 \
     -optimizer SGD \
-    -learning_rate 4e-2 \
+    -learning_rate 0.04 \
     -momentum 0.6 \
+    -learning_rate_decay 0 \
 \
-    -load_model true \
-    -path2load ./sk5/ \
+    -load_model false \
+    -path2load ./sk7/ \
     -epoch 100 \
     -path2save ./sk7/ \
     -plot_every 2000 \
@@ -30,5 +31,6 @@ th -i main.lua \
     -test true \
     -save true 
     
+
     
     
