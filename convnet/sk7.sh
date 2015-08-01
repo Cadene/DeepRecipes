bfa36f4f7285a4740975c0cfb31ecc9551792af0
Tamazy <tamazy@laposte.net>
2015-08-02 00:52:30 +0200

    sync

--- convnet/sk7.sh
+++ convnet/sk7.sh
@@ -1,7 +1,7 @@
 th -i main.lua \
 \
     -data_type Recipe101 \
-    -path2dir /home/cadene/data/recipe_101_clean/ \
+    -path2dir /home/cadene/data/recipe_101_tiny/ \
 \
     -pc_train 0.8 \
 \
@@ -14,19 +14,19 @@ th -i main.lua \
 \
     -batch_size 128 \
     -optimizer SGD \
-    -learning_rate 0.04 \
+    -learning_rate 0.01 \
     -momentum 0.6 \
     -learning_rate_decay 0 \
 \
-    -load_model false \
+    -load_model true \
     -path2load ./sk7/ \
     -epoch 100 \
-    -path2save ./sk7/ \
+    -path2save ./sk7-1/ \
     -plot_every 2000 \
     -plot false \
     -save_every 1 \
 \
-    -run true \
+    -run false \
     -train true \
     -test true \
     -save true 
