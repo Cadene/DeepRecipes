cc98e92d82cf2e53dc3b3d6f36aada8cb61f3c07
Tamazy <tamazy@laposte.net>
2015-08-27 16:30:04 +0200

    fix

--- convnet/src/Model.lua
+++ convnet/src/Model.lua
@@ -1,7 +1,7 @@
 require 'torch'
 require 'optim'
 
-require 'src/ConfusionMatrix'
+require 'src/ConfusionMatrixMonitored'
 
 local Model = torch.class('Model')
 
@@ -39,7 +39,7 @@ function Model:train(database, criterion, optimizer, logger, opt, epoch)
     epoch = epoch or 0
     local timer = torch.Timer()
     local trainset = database:get_trainset()
-    local confusion = ConfusionMatrix(database:nb_class())
+    local confusion = ConfusionMatrixMonitored(database:nb_class())
     local nb_batch = 1
     local nb_batch_max = math.ceil(trainset:size() / opt.batch_size)
     local pc_max = {0, 0}
