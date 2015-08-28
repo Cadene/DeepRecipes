eecf1e6ed6e293d5afc56c755e4fff806b969ea4
Tamazy <tamazy@laposte.net>
2015-08-28 15:41:27 +0200

    final fix

--- convnet/overfeat_lr1.sh
+++ convnet/overfeat_lr1.sh
@@ -1,18 +1,24 @@
+gate
+ssh kepler
+convnet
+
 gpu=0
-lr=1e-3
+lr=2e-2
 loadm="false"
+path2load="/Users/remicadene/data/recipe_101_tiny/"
+#path2load="/home/cadene/data/recipe_101_clean/"
 
 name="overfat+$gpu+$lr" ; echo $name
 path2save="./rslt/$name"
 mkdir $path2save
 echo "CUDA_VISIBLE_DEVICES=$gpu th main.lua \
     -type_data recipe101 \
-    -path2load_data /home/cadene/data/recipe_101_clean/ \
+    -path2load_data $path2load \
     -process_mean_std false \
     -prepare_data false \
     -gpuid 1 \
-    -cuda true \
-    -cudnn true \
+    -cuda false \
+    -cudnn false \
     -4d_tensor true \
     -type_model overfeat \
     -pretrain_model false \
@@ -27,13 +33,11 @@ echo "CUDA_VISIBLE_DEVICES=$gpu th main.lua \
     -save_model true \
     -load_model $loadm \
     -path2load $path2save" > $name.sh
-chmod 777 $name.sh #; ./$name.sh
+chmod 777 $name.sh ; ./$name.sh
 nohup ./$name.sh > $path2save/run.log &
 cat $path2save/run.log
 
-gate
-ssh kepler
-convnet
+
 
 
 gpu=0
