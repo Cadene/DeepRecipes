4bbe1d812409f593b61b09c257395755bfeef4b8
Tamazy <tamazy@laposte.net>
2015-08-31 11:01:48 +0200

    fix

--- convnet/src/sgd.lua
+++ convnet/src/sgd.lua
@@ -83,6 +83,7 @@ return function (opfunc, x, config, state)
       x:add(-clr, state.deltaParameters)
       --print('x_after', x[1], 'x_last', x[last])
    else
+      print('sgd', clr, dfdx[{{1,100}}]:mean())
       x:add(-clr, dfdx)
    end
 
